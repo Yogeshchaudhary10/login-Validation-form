d81063b07be32c1ad4619d0a9f9fb9e1efe7f646
Yogeshchaudhary10 <68645166+Yogeshchaudhary10@users.noreply.github.com>
2023-09-21 18:59:03 +0545

    hello

---
+++ lib/login_form/forms.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class forms extends StatefulWidget {
+  const forms({super.key});
+
+  @override
+  State<forms> createState() => _formsState();
+}
+
+class _formsState extends State<forms> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
